5a01ca970d0dc6dd3c85b623aae1e2a27c896a4e
Samuel Romero Arbelo <samuelromeroarbelo@gmail.com>
2026-06-17 10:33:17 +0100

    Make equipment admin policy drop idempotent

--- supabase/migrations/20260602000007_kim391_fix_reservation_equipment_admin_policy_binding.sql
+++ supabase/migrations/20260602000007_kim391_fix_reservation_equipment_admin_policy_binding.sql
@@ -3,7 +3,7 @@
 -- Change admin_all policy from TO authenticated (which ORs with 4 other policies)
 -- to TO service_role, ensuring admin mutations use the dedicated admin client path.
 
-DROP POLICY "reservation_equipment_admin_all" ON "public"."reservation_equipment";
+DROP POLICY IF EXISTS "reservation_equipment_admin_all" ON "public"."reservation_equipment";
 
 CREATE POLICY "reservation_equipment_admin_all" ON "public"."reservation_equipment"
   FOR ALL
